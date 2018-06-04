06856857863e8b2ae9da65dfb031535d6ea75e99
3went2 <webb.png@gmail.com>
2018-06-04 16:22:53 +0800

    update demo

--- demo/App.js
+++ demo/App.js
@@ -64,7 +64,7 @@ export default class App extends Component {
 					validation={(val) => /a/.test(val)}
 					required
 				/>
-				<Input label="height" name="height" dataType="integer" />
+				<Input label="height" name="height" format="integer" />
 				<ArrayOf name="starships">
 					{(starships, { push, removeBy }) => (
 						<div>
